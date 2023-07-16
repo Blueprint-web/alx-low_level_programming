8391122621629892ac65dcd0cbb3736a9f9d7ea2
James <ugwuayijameson@gmail.com>
2023-07-16 19:19:52 +0000

    variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (b > a)
+			{
+
+				putchar(a);
+				putchar(b);
+				if (a != 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}

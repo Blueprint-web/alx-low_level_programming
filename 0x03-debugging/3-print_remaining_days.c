5a7d073f6ad016954737fa67bda10410fc52a4fe
Jameson <ugwuayijameson@gmail.com>
2023-07-21 16:26:03 +0100

    the day issue

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,38 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || year % 400 == 0)
+	if ((month > 0 && month < 13) && (day > 0 && day < 32))
 	{
-		if (month >= 2 && day >= 59)
+		if ((year % 4 == 0 || year % 400 == 0) && (month > 0 || month < 13))
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
-			day++;
-		}
-
-	}
-	else
-	{
-		if ((month == 2 && day == 60) && !(year % 4 == 0 || year % 400 == 0))
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			if (month >= 2 && day >= 59)
+			{
+				printf("Day of the year: %d\n", day);
+				printf("Remaining days: %d\n", 366 - day);
+				day++;
+			} else
+			{
+				printf("Day of the year: %d\n", day);
+				printf("Remaining days: %d\n", 366 - day);
+			}
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			if ((month == 2 && day == 60) && !(year % 4 == 0 || year % 400 == 0))
+			{
+				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			}
+			else
+			{
+				if ((month > 0 && month < 13 ) && (day > 0 && day < 32))
+				{
+					printf("Day of the year: %d\n", day);
+					printf("Remaining days: %d\n", 365 - day);
+				}
+			}
 		}
+	} else
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 }

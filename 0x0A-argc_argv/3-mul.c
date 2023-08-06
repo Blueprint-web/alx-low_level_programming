3b4dd4b1380517dfb931f4da03a9792f1ca844bd
Jameson <ugwuayijameson@gmail.com>
2023-08-07 00:15:23 +0100

    .

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,12 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 1;
 
-	if (argc == 1 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	
 	else
 	{
 		for (i = 1; i < argc; i++)
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,8 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, sum;
 
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (1);
+	}
+	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
 		if (atoi(argv[i]))

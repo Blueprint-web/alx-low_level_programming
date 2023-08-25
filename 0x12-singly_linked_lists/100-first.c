ea356606c77271dbceef86a448f58773a0c7a1cd
jameson <ugwuayijameson@gmail.com>
2023-08-25 03:36:14 +0100

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print - to print word
+ *
+ * Return: void
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
